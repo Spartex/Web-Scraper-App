1cbd9e52bbd1d71cc343ffb79cea2f95acab16fb
John Comodikes <johncomodikes@Johns-MacBook-Pro.local>
2018-10-29 13:42:55 -0700

    Adding Initial FIles

---
+++ public/assets/css/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  h4: {
+    color: 'white'
+  },
+  hover: {
+    backgroundColor: '#F26232'
+  },
+  'hoverRed:hover': {
+    color: '#000000'
+  },
+  'hover:hover': {
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  }
+});
